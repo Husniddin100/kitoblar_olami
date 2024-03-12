72f988fa5fc2d6b386cabe8b7e9cc200d5b539e4
Husniddin <uchanal10@gmail.com>
2024-03-12 11:21:24 +0500

    first commit

---
+++ src/main/java/com/example/kitoblar_olami/KitoblarOlamiApplication.java
@@ -0,0 +1,11 @@
+package com.example.kitoblar_olami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitoblarOlamiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KitoblarOlamiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/kitoblar_olami/KitoblarOlamiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kitoblar_olami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitoblarOlamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
